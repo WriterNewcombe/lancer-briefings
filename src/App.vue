cd04a16dd7563cf8360d3764311815f934f7ced3
AlexanderNewcombe <alexbnewcombe@gmail.com>
2023-01-25 22:08:31 -0500

    Partial add of Mountain mech and portrait

--- src/App.vue
+++ src/App.vue
@@ -101,12 +101,12 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Mountain",
+          "alias": "Kedra Yolanovich",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "mech": "Fistbreaker"
         },
         {
           "callsign": "Kal",
