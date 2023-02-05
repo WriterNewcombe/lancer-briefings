75607b436871d15e091d1fa665342a427d5f143a
AlexanderNewcombe <alexbnewcombe@gmail.com>
2023-02-05 16:51:45 -0500

    re-adding placeholder 2nd mission

--- src/App.vue
+++ src/App.vue
@@ -96,6 +96,11 @@ export default {
         {
           "slug": "001",
           "name": "Bug-Hunt",
+          "status": "success"
+        },
+        {
+          "slug": "002",
+          "name": "Vigilant Gaze",
           "status": "start"
         },
       ],
